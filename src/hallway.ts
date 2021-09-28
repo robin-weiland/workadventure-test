43fc091d6f63d5b1712fc1ac48cbfae4d1217b8b
florikoli <50080173+florikoli@users.noreply.github.com>
2021-09-28 20:22:30 +0000

    Deploying to gh-pages from master @ 1ec7c96a9427f643cc66d0327fccb6d5a8c8b0b5 🚀

---
+++ src/hallway.ts
@@ -0,0 +1,21 @@
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+
+import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+
+// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
+bootstrapExtra().catch(e => console.error(e));
+
+let currentPopup: any = undefined;
+
+WA.room.onEnterZone('info_room_one', () => {
+    currentPopup =  WA.ui.openPopup("info_room_three_popup","Go to Demo Room", []);
+})
+
+WA.room.onLeaveZone('info_room_one', closePopUp)
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
